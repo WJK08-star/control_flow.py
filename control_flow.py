103618e770d9baf86eb414e42301274268d608ea
WJK08-star <wjk6@icloud.com>
2025-09-15 09:00:47 -0400

    Add files via upload

---
+++ control_flow.py
@@ -0,0 +1,29 @@
+# This program will convert the weight on each planet
+
+
+user_input = int(input("Hello, what is the weight you would like to convert?"))
+weight_on_earth= user_input
+# Planet  Relative
+
+def space_converter():
+ weight_mercury=  90
+ weight_venus =   91
+ weight_mars =    38
+ weight_Jupiter = 234
+ Satum =   106
+ Uranus =  92
+ Neptune = 119
+ Pluto =   6.3 
+
+
+ weight_on_earth % Mercury 
+ weight_on_earth % Venus 
+ weight_on_earth % Mars 
+ weight_on_earth % Jupiter  
+ weight_on_earth % Satum 
+ weight_on_earth % Uranus 
+ weight_on_earth % Neptune 
+ weight_on_earth % Pluto 
+
+
+space_converter()
