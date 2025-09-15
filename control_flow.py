4621bd49fac17dfadbe4c7d388c2b14a453bebea
WJK08-star <wjk6@icloud.com>
2025-09-15 09:11:02 -0400

    Add files via upload

--- control_flow.py
+++ control_flow.py
@@ -9,21 +9,28 @@ def space_converter():
  weight_mercury=  90
  weight_venus =   91
  weight_mars =    38
- weight_Jupiter = 234
- Satum =   106
- Uranus =  92
- Neptune = 119
- Pluto =   6.3 
+ weight_jupiter = 234
+ weight_satum =   106
+ weight_uranus =  92
+ weight_neptune = 119
+ weight_pluto =   6.3 
 
 
- weight_on_earth % Mercury 
- weight_on_earth % Venus 
- weight_on_earth % Mars 
- weight_on_earth % Jupiter  
- weight_on_earth % Satum 
- weight_on_earth % Uranus 
- weight_on_earth % Neptune 
- weight_on_earth % Pluto 
+ weight_on_earth %weight_mercury 
+ weight_on_earth % weight_venus 
+ weight_on_earth % weight_mars
+ weight_on_earth % weight_jupiter  
+ weight_on_earth % weight_satum
+ weight_on_earth % weight_uranus
+ weight_on_earth % weight_neptune
+ weight_on_earth % weight_pluto
 
-
+ print(f"Mercury = {weight_on_earth %weight_mercury}")
+ print(f"Venus = {weight_on_earth % weight_venus} ")
+ print(f"Mars = {weight_on_earth % weight_mars}")
+ print(f"Jupiter = {weight_on_earth % weight_jupiter}")
+ print(f"Satum = {weight_on_earth % weight_satum}")
+ print(f"Uranus = {weight_on_earth % weight_uranus}")
+ print(f"Neptune = {weight_on_earth % weight_neptune}")
+ print(f"Pluto = {weight_on_earth % weight_pluto}")
 space_converter()
