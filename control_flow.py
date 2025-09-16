5af31f6a0a5f45d435fd0b3eef5ff2f7fbe6e861
WJK08-star <wjk6@icloud.com>
2025-09-16 09:41:25 -0400

    Add files via upload

--- control_flow.py
+++ control_flow.py
@@ -3,25 +3,25 @@
 
 user_input = int(input("Hello, what is the weight you would like to convert?"))
 weight_on_earth= user_input
-# Planet  Relative
+
 
 def space_converter():
- weight_mercury=  90
- weight_venus =   91
- weight_mars =    38
- weight_jupiter = 234
- weight_satum =   106
- weight_uranus =  92
- weight_neptune = 119
- weight_pluto =   6.3 
+ weight_mercury=  0.38
+ weight_venus =   0.91
+ weight_mars =   0.38
+ weight_jupiter = 2.34
+ weight_satum =   1.06
+ weight_uranus =  0.92
+ weight_neptune = 1.19
+ weight_pluto =   0.063 
 
 
- print(f"Mercury = {weight_on_earth % weight_mercury} lb")
- print(f"Venus = {weight_on_earth % weight_venus} lb")
- print(f"Mars = {weight_on_earth % weight_mars}")
- print(f"Jupiter = {weight_on_earth % weight_jupiter} lb")
- print(f"Satum = {weight_on_earth % weight_satum}")
- print(f"Uranus = {weight_on_earth % weight_uranus}lb")
- print(f"Neptune = {weight_on_earth % weight_neptune}lb")
- print(f"Pluto = {weight_on_earth % weight_pluto}lb")
+ print(f"Mercury = {weight_on_earth * weight_mercury}lb")
+ print(f"Venus = {weight_on_earth * weight_venus}lb")
+ print(f"Mars = {weight_on_earth * weight_mars}")
+ print(f"Jupiter = {weight_on_earth * weight_jupiter}lb")
+ print(f"Satum = {weight_on_earth * weight_satum}")
+ print(f"Uranus = {weight_on_earth * weight_uranus}lb")
+ print(f"Neptune = {weight_on_earth * weight_neptune}lb")
+ print(f"Pluto = {weight_on_earth * weight_pluto}lb")
 space_converter()
